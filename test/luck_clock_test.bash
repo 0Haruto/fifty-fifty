e52c8c6a24251bcc3708666b1f209180d3c543d0
Haruto Yamamoto <s23c1146pp@s.chibakoudai.jp>
2025-01-03 23:27:52 +0900

    add

--- test/luck_clock_test.bash
+++ test/luck_clock_test.bash
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# ディレクトリの設定
 dir=~
 [ "$1" != "" ] && dir="$1"
 
+# ROS 2ワークスペースに移動
 cd $dir/ros2_ws
-colconbuild
-source $dir/.bashrc
-timeout 10 ros2 run fifty_fifty luck_clock > /tmp/fifty_fifty.log
 
-cat /tmp/fifty_fifty.log |
-	grep 'Listen: 10'
+# colcon buildの実行
+colcon build
+
+# .bashrcのソース
+source $dir/.bashrc
+
+# ノードの実行とログの保存
+timeout 5 ros2 run fifty_fifty luck_clock > /tmp/fifty_fifty.log
+
+# ログファイルから特定の文字列を検索して表示
+grep 'Dead' /tmp/fifty_fifty.log || grep 'Alive' /tmp/fifty_fifty.log
+
